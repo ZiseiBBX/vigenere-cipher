4514305261913ef52acb2022c0ab7470833cff3d
Zisei <zisei.games@gmail.com>
2022-01-20 15:34:55 +0530

    Removed settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
-import {
-	Button,
-	Flex,
-	Text,
-	VStack,
-	HStack,
-	Textarea,
-	Box,
-	Modal,
-	ModalOverlay,
-	ModalContent,
-	ModalHeader,
-	ModalFooter,
-	ModalBody,
-	ModalCloseButton,
-	useDisclosure,
-	Select,
-	useToast,
-} from "@chakra-ui/react";
+import { Button, Flex, Switch, Text, VStack, HStack, Textarea, Box, Select, useToast } from "@chakra-ui/react";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
-import { ArrowDownIcon, SettingsIcon } from "@chakra-ui/icons";
-import { primaryLightColor } from "./theme";
-import Settings from "./components/Settings";
 import { performVigenereEncrypt } from "./utils/vigenereFunctions/encryption";
 import { performVigenereDecrypt } from "./utils/vigenereFunctions/decryption";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { Characters } from "./utils/constants";
 import { MotionHeading, MotionFlex, MotionText } from "./utils/motionComponents";
 
@@ -54,7 +33,6 @@ function App() {
 	const encryptRef = useRef<HTMLTextAreaElement>(null);
 	const decryptRef = useRef<HTMLTextAreaElement>(null);
 
-	const { isOpen, onOpen, onClose } = useDisclosure();
 	const toast = useToast();
 
 	const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -80,7 +58,7 @@ function App() {
 	};
 
 	useEffect(() => {
-		let characters = Characters.ALPHA;
+		let characters = Characters.ALPHA.slice(1);
 		let index = 0;
 
 		let interval = window.setInterval(() => {
@@ -99,128 +77,89 @@ function App() {
 	}, []);
 
 	return (
-		<Flex height="100vh" width="100%" direction="column">
-			<Box ml="auto" mt="2" mr="2" onClick={onOpen} cursor="pointer">
-				<SettingsIcon w={[6, 6, 8]} h={[6, 6, 8]} color={primaryLightColor} />
-			</Box>
-			<Flex justify="center" align="center" w="100%" flex="1" textAlign="center" px={[2, 0]}>
-				<VStack spacing="1rem">
-					<Box height="80px">
-						<AnimatePresence exitBeforeEnter={true}>
-							{title !== "" && (
-								<MotionHeading
-									key="title"
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1, transition: { duration: 1 } }}
-									exit={{ opacity: 0.05, transition: { duration: 1 } }}
-									fontSize={["2.5rem", "3rem", "4rem"]}
-									color="teal.500"
-									userSelect="none"
-								>
-									{title}
-								</MotionHeading>
-							)}
-						</AnimatePresence>
-					</Box>
-					<HStack spacing="2rem" justify="center">
-						{mode === "encrypt" && (
-							<Textarea
-								ref={encryptRef}
-								value={data.plainText}
-								name="plainText"
-								placeholder="Plain Text"
-								resize="none"
-								onChange={handleInputChange}
-								w={["40%", "auto"]}
-							/>
+		<Flex height="100vh" width="100%" direction="column" justify="center" align="center">
+			<VStack spacing="1rem">
+				<Box height={["2.5rem", "3rem", "4rem"]} mb="2">
+					<AnimatePresence exitBeforeEnter={true}>
+						{title !== "" && (
+							<MotionHeading
+								key="title"
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1, transition: { duration: 1 } }}
+								exit={{ opacity: 0.05, transition: { duration: 1 } }}
+								fontSize={["2.5rem", "3rem", "4rem"]}
+								color="teal.500"
+								userSelect="none"
+							>
+								{title}
+							</MotionHeading>
 						)}
-						{mode === "decrypt" && (
-							<Textarea
-								ref={decryptRef}
-								value={data.cipherText}
-								name="cipherText"
-								placeholder="Cipher Text"
-								resize="none"
-								onChange={handleInputChange}
-								w={["40%", "auto"]}
-							/>
-						)}
+					</AnimatePresence>
+				</Box>
+				<HStack spacing="2rem" justify="center">
+					{mode === "encrypt" && (
 						<Textarea
-							value={data.key}
-							name="key"
-							placeholder="Key"
+							ref={encryptRef}
+							value={data.plainText}
+							name="plainText"
+							placeholder="Plain Text"
 							resize="none"
 							onChange={handleInputChange}
 							w={["40%", "auto"]}
 						/>
-					</HStack>
-					<HStack>
-						<Select
-							value={mode}
-							onChange={(e) => {
-								setMode(e.target.value as Mode);
-								setData({ ...data, plainText: "", cipherText: "", result: "" });
-								if ((e.target.value as Mode) === "encrypt") encryptRef.current?.focus();
-								else decryptRef.current?.focus();
-							}}
-						>
-							<option value="encrypt">Encrypt</option>
-							<option value="decrypt">Decrypt</option>
-						</Select>
-						<Button
-							onClick={() => {
-								setData({ ...data, result: "" });
-								if (mode === "encrypt") performEncryption();
-								else performDecryption();
-							}}
-						>
-							GO!
-						</Button>
-					</HStack>
-					{mode === "encrypt" && (
-						<MotionFlex whileHover={{ scale: 1.1 }}>
-							{[...data.plainText].map((letter, index) => (
-								<MotionText
-									key={`${letter}${index}`}
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1, transition: { staggerChildren: 0.5 } }}
-									mx="2"
-									fontWeight="bold"
-									fontSize="1.5rem"
-								>
-									{letter}
-								</MotionText>
-							))}
-						</MotionFlex>
 					)}
 					{mode === "decrypt" && (
-						<MotionFlex whileHover={{ scale: 1.1 }}>
-							{[...data.cipherText].map((letter, index) => (
-								<MotionText
-									key={`${letter}${index}`}
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1, transition: { staggerChildren: 0.5 } }}
-									mx="2"
-									fontWeight="bold"
-									fontSize="1.5rem"
-								>
-									{letter}
-								</MotionText>
-							))}
-						</MotionFlex>
+						<Textarea
+							ref={decryptRef}
+							value={data.cipherText}
+							name="cipherText"
+							placeholder="Cipher Text"
+							resize="none"
+							onChange={handleInputChange}
+							w={["40%", "auto"]}
+						/>
 					)}
-					{data.result && (
-						<MotionFlex whileHover={{ scale: 1.1 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-							<ArrowDownIcon color="red.500" fontSize="1.5rem" />
-						</MotionFlex>
-					)}
+					<Textarea
+						value={data.key}
+						name="key"
+						placeholder="Key"
+						resize="none"
+						onChange={handleInputChange}
+						w={["40%", "auto"]}
+					/>
+				</HStack>
+
+				<HStack>
+					<Select
+						value={mode}
+						onChange={(e) => {
+							setMode(e.target.value as Mode);
+							setData({ ...data, plainText: "", cipherText: "", result: "" });
+							if ((e.target.value as Mode) === "encrypt") encryptRef.current?.focus();
+							else decryptRef.current?.focus();
+						}}
+					>
+						<option value="encrypt">Encrypt</option>
+						<option value="decrypt">Decrypt</option>
+					</Select>
+					<Button
+						onClick={() => {
+							setData({ ...data, result: "" });
+							if (mode === "encrypt") performEncryption();
+							else performDecryption();
+						}}
+					>
+						GO!
+					</Button>
+				</HStack>
+				{mode === "encrypt" && (
 					<MotionFlex whileHover={{ scale: 1.1 }}>
-						{[...data.result].map((letter, index) => (
+						{[...data.plainText].map((letter, index) => (
 							<MotionText
 								key={`${letter}${index}`}
-								mx="2"
 								initial={{ opacity: 0 }}
-								animate={{ opacity: 1, transition: { staggerChildren: 0.5, duration: 1, delay: index * 0.125 } }}
+								animate={{ opacity: 1, transition: { staggerChildren: 0.5 } }}
+								mx="2"
 								fontWeight="bold"
 								fontSize="1.5rem"
 							>
@@ -228,23 +167,49 @@ function App() {
 							</MotionText>
 						))}
 					</MotionFlex>
-				</VStack>
-			</Flex>
-			<Modal isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Settings</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>
-						<Settings />
-					</ModalBody>
-					<ModalFooter>
-						<Button colorScheme="blue" mr={3} onClick={onClose}>
-							Close
-						</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
+				)}
+				{mode === "decrypt" && (
+					<MotionFlex whileHover={{ scale: 1.1 }}>
+						{[...data.cipherText].map((letter, index) => (
+							<MotionText
+								key={`${letter}${index}`}
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1, transition: { staggerChildren: 0.5 } }}
+								mx="2"
+								fontWeight="bold"
+								fontSize="1.5rem"
+							>
+								{letter}
+							</MotionText>
+						))}
+					</MotionFlex>
+				)}
+				{data.result && (
+					<MotionFlex whileHover={{ scale: 1.25 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+						<Box
+							border="solid red"
+							borderWidth="0 3px 3px 0"
+							display="inline-block"
+							padding="3px"
+							transform="rotate(45deg)"
+						></Box>
+					</MotionFlex>
+				)}
+				<MotionFlex whileHover={{ scale: 1.1 }}>
+					{[...data.result].map((letter, index) => (
+						<MotionText
+							key={`${letter}${index}`}
+							mx="2"
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1, transition: { staggerChildren: 0.5, duration: 1, delay: index * 0.125 } }}
+							fontWeight="bold"
+							fontSize="1.5rem"
+						>
+							{letter}
+						</MotionText>
+					))}
+				</MotionFlex>
+			</VStack>
 		</Flex>
 	);
 }
